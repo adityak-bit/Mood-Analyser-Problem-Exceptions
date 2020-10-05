6c020925c4c87c51ce207793c99f3606c1fc36d1
adityak-bit <adityajsr1998@gmail.com>
2020-10-05 07:38:03 +0530

    [ADITYA] Added : NullPointerException handled

--- src/main/java/com/capgemini/map/MoodAnalyser.java
+++ src/main/java/com/capgemini/map/MoodAnalyser.java
@@ -2,20 +2,21 @@ package com.capgemini.map;
 
 public class MoodAnalyser {
 	
+	
 	private String message;
 
-	public MoodAnalyser() {
-	}
-	
 	public MoodAnalyser(String message) {
 		this.message = message;
 	}
 
 	public String analyseMood() {
-		if (message.contains("happy")) {
-			return "SAD";
-		} else {
-			return "HAPPY";
+		try {
+			if (message.contains("happy"))
+				return "HAPPY";
+			else
+				return "SAD";
+		} catch (NullPointerException e) {
+			return "NULL";
 		}
 	}
 }
--- src/test/java/com/capgemini/map/MoodAnalyserTest.java
+++ src/test/java/com/capgemini/map/MoodAnalyserTest.java
@@ -5,12 +5,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
+	
 	@Test
 	public void testMoodAnalysis() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
+		
+			MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
+			MoodAnalyser moodAnalyser1 = new MoodAnalyser(null);
 
-		String mood = moodAnalyser.analyseMood();
+			String mood = moodAnalyser.analyseMood();
+			String mood1 = moodAnalyser1.analyseMood();
 
-		Assert.assertThat(mood, CoreMatchers.is("SAD"));
+			Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
+			Assert.assertThat(mood1, CoreMatchers.is("NULL"));    
 	}
 }
