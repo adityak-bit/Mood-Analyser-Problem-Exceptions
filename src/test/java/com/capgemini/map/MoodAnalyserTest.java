daeec6d99f75eb9b33b5b6e348f1d0dc5154392d
adityak-bit <adityajsr1998@gmail.com>
2020-10-05 13:52:58 +0530

    [ADITYA] Custom exception added- testcase3.2

--- src/test/java/com/capgemini/map/MoodAnalyserTest.java
+++ src/test/java/com/capgemini/map/MoodAnalyserTest.java
@@ -1,19 +1,44 @@
 package com.capgemini.map;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
 
 	@Test
-	public void testMoodAnalysis() {
-
+	public void TestMoodAnalysisEnteredEmpty() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("");
+		try {
+		moodAnalyser.analyseMood();
+		} catch (MoodAnalyserException e) {
+			Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, e.type);
+		}
+	}
+	@Test
+	public void TestMoodAnalysisEnteredNull() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
 		try {
-			moodAnalyser.analyseMood();
+		moodAnalyser.analyseMood();
 		} catch (MoodAnalyserException e) {
 			Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.type);
 		}
+	}
+	@Test
+	public void TestMoodAnalysisAnyMood() throws MoodAnalyserException{
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("I am in Any mood");
+		Assert.assertEquals("HAPPY", mood);
+	}
+	@Test
+	public void TestMoodAnalysisSadMood() throws MoodAnalyserException{
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("I am in sad mood");
+		Assert.assertEquals("SAD", mood);
+	}
+	@Test
+	public void TestMoodAnalysisHappyMood() throws MoodAnalyserException{
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("I am in happy mood");
+		Assert.assertEquals("HAPPY", mood);
 	}
 }
